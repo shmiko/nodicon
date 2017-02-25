94787974b82ed9f81c298c6edc0bc26a0d9488ac
Paul Jones <pjones@calmapit.com>
2017-02-25 17:58:11 +1100

    filename typo

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
-const http = require('http');
-const url = require('url');
-const path = require('path');
-const fs = require('fs');
+const http 	= require('http');
+const url 	= require('url');
+const path 	= require('path');
+const fs 	= require('fs');
 
 const mimeTypes = {
-	"html":"text/html",
-	"jpeg":"image/jpeg",
-	"jpg":"image/jpg",
-	"png":"image/png",
-	"js":"text/javascript",
-	"css":"text/css"
+	"html"	:"text/html",
+	"jpeg"	:"image/jpeg",
+	"jpg"	:"image/jpg",
+	"png"	:"image/png",
+	"js"	:"text/javascript",
+	"css"	:"text/css"
 }
 
 http.createServer(function(req,res){
